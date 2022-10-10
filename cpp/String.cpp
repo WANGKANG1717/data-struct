198a59f2525664c6ff477c110a8d437d2ec53365
WANGKANG <1686617586@qq.com>
2022-10-10 15:10:05 +0800

    重新设置文件夹的布局，更改vscode的编译位置 满足了我的强迫症，哈哈啊哈哈

---
+++ cpp/String.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<cstdio>
+#include<queue>
+#include<cstring>
+#include<string>
+#include<vector>
+#include<map>
+#include<stack>
+#include<set>
+#include<algorithm>
+#include<cstdlib>
+#include<cctype>
+#include<cmath>
+using namespace std;
+#define cio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define _T int _T; cin>>_T; while(_T--)
+#define MAXN 100010
+#define MAXINT 0x7f7f7f7f
+#define _MAXINT 0x3f3f3f3f
+#define debug(x) cout<<#x<<"="<<x<<endl
+#define pii pair<int,int>
+#define mk make_pair
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+const int mod = 1e9 + 7;
+
+
+
+int main() {
+    cio;
+    cout<<"12122"<<endl;
+    
+    
+    return 0;
+}
---
+++ head/String.h
@@ -0,0 +1,13 @@
+/**
+ * @author: WangKang
+ * @blog: kang17.xyz
+ * @filename: String.h
+ * @filepath: head\String.h
+ * @date: 2022-10-10 14:35:21
+ * @description: 字符串类
+ */
+
+#ifndef __STRING_H__
+#define __STRING_H__
+
+#endif
