0a20ac111d8a0daa731747eb4ca13e5642e4e1a1
WANGKANG <1686617586@qq.com>
2022-10-14 14:52:56 +0800

    树的大多数算法都已实现

--- cpp/BiTree.cpp
+++ cpp/BiTree.cpp
@@ -42,9 +42,60 @@ const int mod = 1e9 + 7;
 int main() {
     cio;
     // data 1 2 4 0 0 5 0 0 3 0 0
-    BiTree<int> T;
-    T.create(0);
-    T.preOrder(T.getRoot());
+    int data[] = {1, 2, 4, 0, 0, 5, 0, 0, 3, 0, 0};
+    int data2[] = {1, 2, 3, 4, 5, 0, 0, 0, 0, 0, 0};
+    //满二叉树
+    int data3[] = {1, 2, 4,  8, 0, 0,  9, 0, 0, 5,  10, 0, 0,  11, 0, 0,
+                   3, 6, 12, 0, 0, 13, 0, 0, 7, 14, 0,  0, 15, 0,  0};
+    int data4[] = {0};
+    BiTree<int> T, T2, T3, T4;
+    // T.create(0);
+    int i = 0;
+    int n = 11;
+    int endFlag = 0;
+    T.create(T.getRoot(), data, endFlag, i, n);
+    i = 0;
+    T2.create(T2.getRoot(), data2, endFlag, i, n);
+    i = 0;
+    T3.create(T3.getRoot(), data3, endFlag, i, 31);
+    //空树
+    i = 0;
+    T4.create(T4.getRoot(), data4, endFlag, i, 1);
+    debug(T.getDepth());
+    debug(T2.getDepth());
+    debug(T3.getDepth());
+    debug(T4.getDepth());
+    // T.preOrder(T.getRoot());
+    // cout << endl;
+    // T.inOrder(T.getRoot());
+    // cout << endl;
+    // T.postOrder(T.getRoot());
+    // cout << endl;
+    // T.preOrder2();
+    // cout << endl;
+    // T.inOrder2();
+    // cout << endl;
+    // T.postOrder2();
+    // cout << endl;
+    // T.levelOrder();
+    // T2.levelOrder();
+    // T2.postOrder(T2.getRoot());
+    // cout << endl;
+    // T2.inOrder2();
+    // cout << endl;
+    // T2.preOrder2();
+    // cout << endl;
+    // T2.preOrder(T2.getRoot());
+    T.postOrder2();
+    cout << endl;
+    T2.postOrder2();
+    cout << endl;
+    T3.postOrder2();
+    T4.postOrder2();
+    // debug(T.isEmpty());
+    // T.clear();
+    // debug(T.isEmpty());
+    //
 
     return 0;
 }
--- head/BiTree.h
+++ head/BiTree.h
@@ -7,11 +7,17 @@
  * @description: 树
  */
 
+/**
+ * @date: 2022-10-14 13:30:26
+ * @description: 指针引用 *& 指针 是有区别的，需要记住
+ */
+
 #ifndef __BITREE_H__
 #define __BITREE_H__
 #include "../head/LinkQueue.h"
 #include "../head/LinkStack.h"
 #include <iostream>
+#include <map>
 using namespace std;
 template <class T> class BiNode {
   public:
@@ -42,62 +48,82 @@ template <class T> class BiTree : public BiNode<T> {
     BiNode<T> *getRightSibing(BiNode<T> *curr, BiNode<T> *target);
 
   public:
-    BiTree();
-    ~BiTree();
-    void clear(BiNode<T> *Root);
+    BiTree();  //
+    ~BiTree(); ////
+    void clear();
+    void Clear(BiNode<T> *&Root); //
     /**
      * @date: 2022-10-13 13:27:03
      * @param: data; i:下标; n: 总的数据大小; endFlag 自定义的结束标志
      * @return: NULL
      * @description: 传一个大小为n的数组，要求前序遍历存储，以#结尾，作为空节点
      */
-    void create(BiNode<T> *Root, T *data, T endFlag, int &i, int n);
+    /**
+     * @date: 2022-10-14 13:31:22
+     * @description:
+     * 搞这个函数的时候，感觉写的很对，死活出不来，原来是指针引需要引用的原因
+     */
+    /**
+     * @date: 2022-10-14 13:55:28
+     * @description: 注
+     * create函数最好再封装一下，这里由于时间问题，直接忽略，能用就行，重点再了解其思想
+     */
+    void create(BiNode<T> *&Root, T *data, T endFlag, int &i, int n); //
     /**
      * @date: 2022-10-13 13:28:50
      * @param: root
      * @return: NULL
      * @description: 根据提示按照前序序列创建二叉树
      */
-    void create(T endFlag);
-    BiNode<T> *Create(T endFlag);
-    bool isEmpty();
-    int getDepth(int dep);
-    BiNode<T> *getRoot();
+    void create(T endFlag);                 //
+    BiNode<T> *Create(T endFlag);           //
+    bool isEmpty();                         //
+    int getDepth();                         //
+    int GetDepth(BiNode<T> *Root, int dep); //
+    BiNode<T> *&getRoot();                  //
     BiNode<T> *parent(BiNode<T> *target);
     BiNode<T> *LeftSibing(BiNode<T> *target);
     BiNode<T> *RightSibing(BiNode<T> *target);
     BiNode<T> *Lchild(BiNode<T> *curr);
     BiNode<T> *Rchild(BiNode<T> *curr);
-    void preOrder(BiNode<T> *Root);
-    void preOrder2();
-    void inOrder(BiNode<T> *Root);
-    void inOrder2();
-    void postOrder(BiNode<T> *Root);
+    void preOrder(BiNode<T> *Root);  //
+    void preOrder2();                //
+    void inOrder(BiNode<T> *Root);   //
+    void inOrder2();                 //
+    void postOrder(BiNode<T> *Root); //
     void postOrder2();
-    void levelOrder();
+    void levelOrder(); //
 };
 template <class T> BiTree<T>::BiTree() : root(NULL) {}
 
-template <class T> BiTree<T>::~BiTree() { clear(root); }
+template <class T> BiTree<T>::~BiTree() { Clear(root); }
 
-template <class T> void BiTree<T>::clear(BiNode<T> *Root) {
-    // if (isEmpty())
-    //     return;
-    // clear(root->lchild);
-    // clear(root->rchild);
-    // delete (root);
+template <class T> void BiTree<T>::clear() {
+    if (root == NULL)
+        return;
+    Clear(root);
+    root = NULL;
+}
+
+template <class T> void BiTree<T>::Clear(BiNode<T> *&r) {
+    if (r == NULL)
+        return;
+    Clear(r->lchild);
+    Clear(r->rchild);
+    delete (r);
 }
 
 template <class T>
-void BiTree<T>::create(BiNode<T> *Root, T *data, T endFlag, int &i, int n) {
+void BiTree<T>::create(BiNode<T> *&Root, T *data, T endFlag, int &i, int n) {
     if (data[i] == endFlag) {
-        root = NULL;
+        Root = NULL;
         return;
     }
-    root = new BiNode<T>(data[i]);
+    Root = new BiNode<T>(data[i]);
     i++;
-    create(root->lchild, data, endFlag, i, n);
-    create(root->rchild, data, endFlag, i, n);
+    create(Root->lchild, data, endFlag, i, n);
+    i++;
+    create(Root->rchild, data, endFlag, i, n);
 }
 /**
  * @date: 2022-10-13 15:18:41
@@ -123,16 +149,16 @@ template <class T> bool BiTree<T>::isEmpty() {
     } else
         return false;
 }
-
-template <class T> int BiTree<T>::getDepth(int dep) {
-    if (root == NULL)
+template <class T> int BiTree<T>::getDepth() { return GetDepth(root, 0); }
+template <class T> int BiTree<T>::GetDepth(BiNode<T> *Root, int dep) {
+    if (Root == NULL)
         return 0;
-    int ldep = getDepth(root->lchild);
-    int rdep = getDepth(root->child);
+    int ldep = GetDepth(Root->lchild, dep + 1);
+    int rdep = GetDepth(Root->rchild, dep + 1);
     return max(ldep, rdep) + 1;
 }
 
-template <class T> BiNode<T> *BiTree<T>::getRoot() { return root; }
+template <class T> BiNode<T> *&BiTree<T>::getRoot() { return root; }
 
 template <class T> BiNode<T> *BiTree<T>::parent(BiNode<T> *target) {
     return getParent(root, NULL, target);
@@ -191,83 +217,109 @@ template <class T> BiNode<T> *BiTree<T>::Rchild(BiNode<T> *curr) {
 
 template <class T> void BiTree<T>::preOrder(BiNode<T> *Root) {
     if (Root) {
-        cout << Root->data << endl;
+        cout << Root->data << " ";
         preOrder(Root->lchild);
         preOrder(Root->rchild);
     }
 }
 template <class T> void BiTree<T>::preOrder2() {
-    cout << "---------------------" << endl;
-    cout << "preOrder2:" << endl;
+    // cout << "---------------------" << endl;
+    // cout << "preOrder2:" << endl;
     LinkStack<BiNode<T> *> S;
     BiNode<T> *p = NULL;
-    if (!isEmpty()) {
-        S.push(root);
+    if (root != NULL) {
         p = root;
-        cout << p->data << " ";
     }
     while (!S.isEmpty() || p) {
-        while (p->lchild) {
-            S.push(p->lchild);
-            p = p->lchild;
+        while (p) {
             cout << p->data << " ";
+            S.push(p);
+            p = p->lchild;
         }
         p = S.getTop();
         S.pop();
         p = p->rchild;
     }
 
-    cout << "\n---------------------" << endl;
+    // cout << "\n---------------------" << endl;
 }
 
 template <class T> void BiTree<T>::inOrder(BiNode<T> *Root) {
     if (Root) {
-        preOrder(Root->lchild);
-        cout << Root->data << endl;
-        preOrder(Root->rchild);
+        inOrder(Root->lchild);
+        cout << Root->data << " ";
+        inOrder(Root->rchild);
     }
 }
 template <class T> void BiTree<T>::inOrder2() {
-    cout << "---------------------" << endl;
-    cout << "InOrder2:" << endl;
+    // cout << "---------------------" << endl;
+    // cout << "InOrder2:" << endl;
     LinkStack<BiNode<T> *> S;
     BiNode<T> *p = NULL;
-    if (!isEmpty()) {
-        S.push(root);
+    if (root != NULL) {
         p = root;
     }
     while (!S.isEmpty() || p) {
-        while (p->lchild) {
-            S.push(p->lchild);
+        while (p) {
+            S.push(p);
             p = p->lchild;
         }
-        p = S.getTop();
-        S.pop();
-        cout << p->data << " ";
-        p = p->rchild;
+        if (!S.isEmpty()) {
+            p = S.getTop();
+            S.pop();
+            cout << p->data << " ";
+            p = p->rchild;
+        }
     }
 
-    cout << "\n---------------------" << endl;
+    // cout << "\n---------------------" << endl;
 }
 
 template <class T> void BiTree<T>::postOrder(BiNode<T> *Root) {
     if (Root) {
-        preOrder(Root->lchild);
-        preOrder(Root->rchild);
-        cout << Root->data << endl;
+        postOrder(Root->lchild);
+        postOrder(Root->rchild);
+        cout << Root->data << " ";
     }
 }
-template <class T> void BiTree<T>::postOrder2() {}
+template <class T> void BiTree<T>::postOrder2() {
+    LinkStack<BiNode<T> *> S;
+    if (root == NULL)
+        return;
+    BiNode<T> *p = root;
+    /**
+     * @date: 2022-10-14 14:13:42
+     * @description: 使用指针引用，不会创建新的指针
+     */
+    map<BiNode<T> *, int> vis;
+    while (!S.isEmpty() || p) {
+        while (p) {
+            S.push(p);
+            p = p->lchild;
+        }
+        if (!S.isEmpty()) {
+            p = S.getTop();
+            vis[p]++;
+            if (vis[p] == 2) {
+                cout << p->data << " ";
+                S.pop();
+                p = NULL; //神来之笔
+            } else {
+                p = p->rchild;
+            }
+        }
+    }
+}
 template <class T> void BiTree<T>::levelOrder() {
-    cout << "---------------------" << endl;
-    cout << "LevelOrder:" << endl;
+    // cout << "---------------------" << endl;
+    // cout << "LevelOrder:" << endl;
     LinkQueue<BiNode<T> *> Q;
     if (root != NULL) {
         Q.EnQueue(root);
     }
+    BiNode<T> *p;
     while (!Q.isEmpty()) {
-        BiNode<T> *p = Q.front();
-        Q.DeQueue();
+        Q.DeQueue(p);
         cout << p->data << " ";
         if (p->lchild) {
             Q.EnQueue(p->lchild);
@@ -276,7 +328,7 @@ template <class T> void BiTree<T>::levelOrder() {
             Q.EnQueue(p->rchild);
         }
     }
-    cout << "\n-----------------------\n";
+    // cout << "\n-----------------------\n";
 }
 
 #endif
