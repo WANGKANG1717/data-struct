b9dd0eb807657db6abe34756fa47e331d4760cbb
WANGKANG <1686617586@qq.com>
2022-10-21 15:05:21 +0800

    二叉搜索树

--- cpp/BST.cpp
+++ cpp/BST.cpp
@@ -6,7 +6,10 @@
  * @date: 2022-10-19 14:47:08
  * @description: 二叉搜索树
  */
-
+/**
+ * @date: 2022-10-20 15:19:02
+ * @description: 二叉树的删除算法好难懂哦，还得好好学习下，必须搞懂！
+ */
 #include <algorithm>
 #include <iostream>
 using namespace std;
@@ -48,7 +51,62 @@ Node *search(Node *T, int x) {
         return search(T->rchild, x);
     }
 }
-void remove(int x) {
+//课本上直接使用数据交换了，我认为不好，试一下手动更换节点
+void swap(Node *p, Node *r, Node *preP, Node *preR) {
+    Node *rl = r->lchild;
+    Node *rr = r->rchild;
+    (preP->lchild == p) ? preP->lchild = r : preP->rchild = r;
+    r->lchild = p->lchild;
+    r->rchild = p->rchild;
+    preR->rchild = p;
+    p->lchild = rl;
+    p->rchild = rr;
+}
+
+bool remove(int x) {
+    Node *preP = NULL;
+    Node *p = BST;
+    while (p->data != x) {
+        preP = p;
+        if (x > p->data) {
+            p = p->rchild;
+        } else {
+            p = p->lchild;
+        }
+    }
+    if (p == NULL) {
+        return false;
+    }
+    //如果被删的节点左右均不空
+    if (p->lchild && p->rchild) {
+        Node *preR = p;
+        Node *r = p->lchild;
+        //开始找最右下的节点
+        while (r->rchild) {
+            preR = r;
+            r = r->rchild;
+        }
+        //交换被删节点与最右下节点
+        // 说明右子树不空
+        if (p->lchild == r) {
+            (preP->lchild == p) ? preP->lchild = r : preP->rchild = r;
+        } else {
+            swap(p, r, preP, preR);
+        }
+        preP = preR;
+    }
+    //注意，这事p的左右子树中一定有一个空的
+    if (preP == NULL)
+        if (p->lchild) {
+            p = p->lchild;
+        } else if (p->rchild) {
+            p = p->rchild;
+        } else {
+            (preP->lchild == p) ? preP->lchild = NULL : preP->rchild = NULL;
+        }
+    // 全部嫁接完成
+    delete (p);
+    if (BST == NULL)
 }
 int main() {
     create();
@@ -66,6 +124,15 @@ int main() {
             cout << p->data << endl;
         else {
             cout << "Not Found!" << endl;
+        }
+    }
+    cout << "请输入要删除的数据（以-1结束）:" << endl;
+    while (cin >> x && x != -1) {
+        bool flag = remove(x);
+        if (flag) {
+            cout << "删除成功！" << endl;
+        } else {
+            cout << "删除失败！" << endl;
         }
     }
 
