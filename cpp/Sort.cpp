0c8af80d34ecb6507ca2d26de2310d873df53941
WANGKANG <1686617586@qq.com>
2022-10-27 14:08:43 +0800

    搞定了三个函数 简单选择排序 希尔排序 快速排序

--- cpp/Sort.cpp
+++ cpp/Sort.cpp
@@ -8,15 +8,26 @@
  */
 #include "../head/sort.h"
 
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 using namespace std;
 
 int main() {
-    int data[10] = {5, 3, 1, 6, 9, 0, 8, 7, 2, 4};
-    // InsertSort(data, 10);
-    BinaryInsertSort(data, 10);
+    srand((int)time(NULL));
+    int data[100000];
+    for (int i = 0; i < 1000; i++) {
+        data[i] = rand() % 10;
+    }
+    // int data[10] = {5, 3, 1, 6, 9, 0, 8, 7, 2, 4};
 
-    for (int i = 0; i < 10; i++) {
+    // insertSort(data, 10);
+    // binaryInsertSort(data, 10);
+    // shellSort(data, 1000);
+    // selectSort(data, 1000);
+    quickSort(data, 1000);
+
+    for (int i = 0; i < 1000; i++) {
         cout << data[i] << " ";
     }
     cout << endl;
--- head/sort.h
+++ head/sort.h
@@ -8,30 +8,22 @@
  */
 #include <iostream>
 using namespace std;
-//插入排序
-/**
- * @date: 2022-10-26 13:40:00
- * @param: data 要排序数据集 n总的数目 func 比较函数
- * @return: NULL
- * @description:
- */
+//插入排序 简单插入排序 二分插入排序 希尔排序
 
 /// @brief 默认的排序函数，就是这么骚气！狗头🐶👌🙌👍
 /// @tparam T 排序类型
-/// @param a
-/// @param b
 /// @return a<b 正数， a==b 0， a<b 负数
 template <class T>
 int cmp(T a, T b) {
     return a < b;
 }
-
+/////////////////////////////////////////////////////////////////////////////////////////////
 /**
  * @date: 2022-10-26 16:02:39
  * @description: 简单插入排序
  */
 template <class T>
-void InsertSort(T data[], int n, int (*cmp)(T a, T b) = cmp) {
+void insertSort(T data[], int n, int (*cmp)(T a, T b) = cmp) {
     for (int i = 1; i < n; i++) {
         T tmp = data[i];
         int j = i;
@@ -49,7 +41,7 @@ void InsertSort(T data[], int n, int (*cmp)(T a, T b) = cmp) {
  * @description: 二分插入排序
  */
 template <class T>
-void BinaryInsertSort(T data[], int n, int (*cmp)(T a, T b) = cmp) {
+void binaryInsertSort(T data[], int n, int (*cmp)(T a, T b) = cmp) {
     for (int i = 1; i < n; i++) {
         int low = 0, high = i - 1;
         T tmp = data[i];
@@ -74,4 +66,89 @@ void BinaryInsertSort(T data[], int n, int (*cmp)(T a, T b) = cmp) {
         // }
         // cout << endl;
     }
+}
+
+/**
+ * @date: 2022-10-27 13:22:32
+ * @description: 希尔排序
+ */
+template <class T>
+void shellSort(T data[], int n, int (*cmp)(T a, T b) = cmp) {
+    for (int k = n / 2; k >= 1; k /= 2) {
+        for (int i = k; i < n; i++) {
+            T tmp = data[i];
+            int j = i - k;
+            for (; j >= 0; j -= k) {
+                if (cmp(tmp, data[j]) > 0) {
+                    data[j + k] = data[j];
+                } else
+                    break;
+            }
+            data[j + k] = tmp;
+        }
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * @date: 2022-10-27 13:45:48
+ * @description: 后面的不再使用cmp比较函数，不够直观
+ *              使用c++的重载比较号 则总是可行的
+ */
+
+//选择排序 简单选择排序 快速排序
+
+/**
+ * @date: 2022-10-27 13:24:39
+ * @description: 简单选择排序
+ */
+template <class T>
+void selectSort(T data[], int n) {
+    for (int i = n - 1; i > 0; i--) {
+        int index = 0;
+        for (int j = 1; j <= i; j++) {
+            if (data[j] > data[index]) {
+                index = j;
+            }
+        }
+        if (index != i) {
+            int tmp = data[index];
+            data[index] = data[i];
+            data[i] = tmp;
+        }
+    }
+}
+
+/**
+ * @date: 2022-10-27 13:30:17
+ * @description: 快速排序
+ */
+
+template <class T>
+void QuickSort(T data[], int l, int r) {
+    if (l >= r) return;
+    int low = l, high = r;
+    T x = data[low];
+    while (low < high) {
+        /**
+         * @date: 2022-10-27 14:06:35
+         * @description: 里面的函数仍然要做边界控制， 否则会出错
+         */
+        while (low < high && data[high] >= x) {
+            high--;
+        }
+        data[low] = data[high];
+        while (low < high && data[low] <= x) {
+            low++;
+        }
+        data[high] = data[low];
+    }
+    data[low] = x;
+    QuickSort(data, l, low - 1);
+    QuickSort(data, low + 1, r);
+}
+
+template <class T>
+void quickSort(T data[], int n) {
+    QuickSort(data, 0, n - 1);
 }
