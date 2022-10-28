435e0c023be9b11fe09d8eb0ec39117a22575314
WANGKANG <1686617586@qq.com>
2022-10-28 15:49:16 +0800

    归并排序搞定 解下来应该还剩三个算法，非递归的归并排序、快速排序，以及基数排序

--- cpp/Sort.cpp
+++ cpp/Sort.cpp
@@ -16,19 +16,28 @@ using namespace std;
 int main() {
     srand((int)time(NULL));
     int data[100000];
-    for (int i = 0; i < 10000; i++) {
-        data[i] = rand() % 10;
+    int n = 100;
+    int random = 200;
+    // int data[] = {1, 8, 9, 1, 2};
+    for (int i = 0; i < n; i++) {
+        data[i] = rand() % random;
     }
-    // int data[10] = {5, 3, 1, 6, 9, 0, 8, 7, 2, 4};
 
     // insertSort(data, 10);
     // binaryInsertSort(data, 10);
     // shellSort(data, 1000);
     // selectSort(data, 1000);
-    quickSort(data, 1000);
+    // quickSort(data, 1000);
     // bubbleSort(data, 10000);
+    // heapSort(data, n);    //调了好久才调出来
+    // for (int i = 0; i < n; i++) {
+    //     int x = rand() % 10;
+    //     data[i] = x;
+    //     adjustHeapBottom2Top(data, i);
+    // }
 
-    for (int i = 0; i < 10000; i++) {
+    mergeSort(data, n);
+    for (int i = 0; i < n; i++) {
         cout << data[i] << " ";
     }
     cout << endl;
--- head/sort.h
+++ head/sort.h
@@ -131,33 +131,76 @@ void selectSort(T data[], int n) {
 // c++不允许函数嵌套定义
 // 堆排序还是相当的优雅的数据结构
 // 可惜只能明天解决了
+#include <iostream>
+using namespace std;
+
 #define lchild(x) ((x)*2 + 1)
 #define rchild(x) ((x)*2 + 2)
+// 这里的x为堆中最后一个元素的下标
 #define parent(x) (((x)-1) / 2)
 template <class T>
-void buildHeap(T data, int n) {
+void printHeap(T data[], int n) {
+    cout << "-----------------Heap Print----------------------" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << data[i] << " ";
+    }
+    cout << endl;
+    cout << "-----------------Heap Print----------------------" << endl;
+}
+template <class T>
+void adjustHeapTop2Bottom(T data[], int n, int p) {
+    T tmp = data[p];
+    // giao 这里应该为n-1  堆中最后一个元素的下标
+    while (p <= parent(n - 1)) {
+        //确定左右子树中最大的一个
+        int child = lchild(p);
+        // child + 1 < n
+        if (child + 1 <= n - 1 && data[child + 1] > data[child]) {
+            child++;
+        }
+        if (tmp > data[child]) break;
+        data[p] = data[child];
+        p = child;
+    }
+    data[p] = tmp;
+}
+/// @brief 从下向上调整堆 插入堆有用
+/// @tparam T
+/// @param data  //堆
+/// @param s   //插入的位置
+template <class T>
+void adjustHeapBottom2Top(T data[], int s) {
+    int tmp = data[s];
+    while (s) {
+        int parent = parent(s);
+        if (tmp < data[parent]) break;
+        data[s] = data[parent];
+        s = parent;
+    }
+    data[s] = tmp;
+}
+///////////////////
+template <class T>
+void buildHeap(T data[], int n) {
     for (int i = parent(n); i >= 0; i--) {
         adjustHeapTop2Bottom(data, n, i);
     }
 }
 template <class T>
-void adjustHeapTop2Bottom(T data, int n, int p) {
-    T tmp = data[p];
-    int s;
-    while (p <= (n - 1)) {
-        if (lchild(p) <= n - 1) {
-            T lc = data[lchild(p)];
-        }
-        if (lchild(p) + 1 <= n - 1) {
-            T rc = data[rchild(p)];
-        }
+void heapSort(T data[], int n) {
+    // printHeap(data, n);
+    buildHeap(data, n);
+    cout << 0 << endl;
+    // printHeap(data, n);
+    for (int i = n - 1; i > 0; i--) {
+        T tmp = data[i];
+        data[i] = data[0];
+        data[0] = tmp;
+        adjustHeapTop2Bottom(data, i, 0);
+        //////////////////
+        // cout << i << endl;
+        // printHeap(data, n);
     }
-}
-template <class T>
-void adjustHeapBottom2Top(T data, int n, int p) {
-}
-template <class T>
-void heapSort(T data, int n) {
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////
@@ -219,4 +262,63 @@ void QuickSort(T data[], int l, int r) {
 template <class T>
 void quickSort(T data[], int n) {
     QuickSort(data, 0, n - 1);
+}
+
+/**
+ * @date: 2022-10-28 15:14:14
+ * @description: 最后两个算法 归并排序 基数排序
+ */
+/**
+ * @date: 2022-10-28 15:35:01
+ * @description: 归并排序异常顺利的写完了！！！
+ */
+#define mid(x, y) (((x) + (y)) / 2)
+/// @brief 归并排序主函数
+/// @tparam T
+/// @param data 数组
+/// @param l 低下标
+/// @param r 高下标
+template <class T>
+void MergeSort(T data[], int l, int r) {
+    if (l >= r) return;
+    /// mid 向下取整
+    int mid = mid(l, r);
+    //先进行小的归并
+    MergeSort(data, l, mid);
+    MergeSort(data, mid + 1, r);
+    //
+    // l ~ mid   mid+1 ~ r
+    int low = l, high = mid + 1, pos = 0;
+    T *tmp = new T[r - l + 1];
+    while (low <= mid && high <= r) {
+        if (data[low] < data[high]) {
+            tmp[pos++] = data[low++];
+        } else {
+            tmp[pos++] = data[high++];
+        }
+    }
+    while (low <= mid) {
+        tmp[pos++] = data[low++];
+    }
+    while (high <= r) {
+        tmp[pos++] = data[high++];
+    }
+    //写回归并好的数据
+    for (int i = l, j = 0; i <= r; i++, j++) {
+        data[i] = tmp[j];
+    }
+}
+
+/// @brief 归并排序
+/// @tparam T
+/// @param data
+/// @param n 数组中的元素个数
+template <class T>
+void mergeSort(T data[], int n) {
+    MergeSort(data, 0, n - 1);
+}
+
+template <class T>
+void mergeSort2(T data[], int n) {
+    for(int i=0; i<)
 }
